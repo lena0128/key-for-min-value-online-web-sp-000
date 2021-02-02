7b27b9ad7e6441838512ac3373f6dae1a7eb3e21
Lena Hou <lenahou2013@gmail.com>
2021-02-02 18:09:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,20 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    puts
-    puts "in key_for_min_value with parameter #{name_hash}"
-#    puts "about to return nil" if name_hash.empty?
-#    return nil if name_hash.empty?
-
-    name_hash.max_by { | item | puts "item=#{item}" }
-
-    max = name_hash.max_by do | k, v |
-        puts "k=#{k}  v=#{v}  0 - v = #{0 - v}"
-        0 - v
+  lowest_key = name_hash.reduce do |key, value|
+    key.last > value.last ? value : key
+  end
+    if name_hash == {}
+      lowest_key
+    else
+      lowest_key.first
     end
-    puts "max=#{max.inspect}, class of value returned by max_by : #{max.class}"
-
-    result = name_hash.max_by {|k, v| 0-v}[0]
-    puts "result=#{result.inspect}"
-    result
 end
